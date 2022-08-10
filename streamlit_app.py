8b81d3c2a84c88ae136d561cb33db31fbdb19aa3
yg-snowflake <110959335+yg-snowflake@users.noreply.github.com>
2022-08-10 16:26:04 -0400

    adding specific fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
