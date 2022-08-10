7c49e462186a2cf853560745d5894f55473bf4d8
yg-snowflake <110959335+yg-snowflake@users.noreply.github.com>
2022-08-10 16:15:44 -0400

    multiselect picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy DIner')
+streamlit.title("My Mom's New Healthy DIner")
 
 streamlit.header('Breakfast Favorites')
 
@@ -17,5 +17,8 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
