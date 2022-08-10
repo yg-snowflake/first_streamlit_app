0c1f0602fab9a9b637eb213efaaf3baba1fb1400
yg-snowflake <110959335+yg-snowflake@users.noreply.github.com>
2022-08-10 00:52:47 -0400

    Added last 2 menu lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title('My Parents New Healthy DIner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
