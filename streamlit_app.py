5a019379a11afbe195998613d929011d8aa4b422
yg-snowflake <110959335+yg-snowflake@users.noreply.github.com>
2022-08-19 12:38:43 -0400

    Asking user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,5 @@ streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
 streamlit.dataframe(my_data_rows)
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
